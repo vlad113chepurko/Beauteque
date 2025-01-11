9fc07872d33a2e4f3e1b47280527384686c27471
vlad113chepurko <vlad113chepurko@gmail.com>
2025-01-11 22:42:13 +0200

    Added Categories, itages, animations, also added interval at 5 seconds for ad banners and nev ad banners images. Also i optimised that's all to phones

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,18 +1,44 @@
 import arrowl from '../assets/ArrowL.svg';
 import arrowr from '../assets/ArrowR.svg';
-import banner from '../assets/banner.jpg';
 import banner1 from '../assets/banner1.jpg';
+import banner2 from '../assets/banner2.jpg';
+
+import girl_face from '../assets/girl-face.jpg'
+import girl_lips from '../assets/girl-lips.jpg'
+import girl_parfume from '../assets/girl-parfume.jpg'
+import girl_acsesuars from '../assets/acsesuars.jpg'
+
 
 import { motion } from "motion/react"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 
 
 export default function Main() {
-
-    const [currentBanner, setCurrentBanner] = useState(banner);
-    const [banners] = useState([banner, banner1]);
+    const [currentBanner, setCurrentBanner] = useState(banner1);
+    const [banners] = useState([banner1, banner2]);
     const [currentSlide, setCurrentSlide] = useState(0);
     const [title, setTitle] = useState('Hit of the month!');
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const nextSlide = currentSlide === 0 ? 1 : 0;
+            setCurrentSlide(nextSlide);
+            switch (nextSlide) {
+                case 0:
+                    setTitle('Hit of the month!');
+                    setCurrentBanner(banner1);
+                    break;
+                case 1:
+                    setTitle('New Collection!');
+                    setCurrentBanner(banner2);
+                    break;
+                default:
+                    break
+            }
+        }, 5000);
+        return () => clearInterval(interval);
+    }, [currentSlide]);
 
     function onClickArrowLeft() {
         const prevSlide = currentSlide === 1 ? 0 : 1;
@@ -20,11 +46,11 @@ export default function Main() {
         switch (prevSlide) {
             case 0:
                 setTitle('Hit of the month!');
-                setCurrentBanner(banner);
+                setCurrentBanner(banner1);
                 break;
             case 1:
                 setTitle('New Collection!');
-                setCurrentBanner(banner1);
+                setCurrentBanner(banner2);
                 break;
             default:
                 break;
@@ -37,11 +63,11 @@ export default function Main() {
         switch (nextSlide) {
             case 0:
                 setTitle('Hit of the month!');
-                setCurrentBanner(banner);
+                setCurrentBanner(banner1);
                 break;
             case 1:
                 setTitle('New Collection!');
-                setCurrentBanner(banner1);
+                setCurrentBanner(banner2);
                 break;
             default:
                 break
@@ -75,8 +101,8 @@ export default function Main() {
                                             />
 
                                             <motion.section
-                                                initial={{  y: -1000 }}
-                                                animate={{  y: 0 }}
+                                                initial={{ y: -1000 }}
+                                                animate={{ y: 0 }}
                                                 transition={{ duration: 1.1, ease: "easeInOut" }}
                                                 className='main-banner__container'>
                                                 <h1 className='main-banner__title'>{title}</h1>
@@ -107,6 +133,58 @@ export default function Main() {
                         ))
                     })}
                 </section>
+
+                <div className='container-categories'>
+
+                    <section className='container-vertical-text'>
+                        <h1 className='vertical-text'>COSMETICS AND PARFUMES</h1>
+                    </section>
+
+                    <section className='container-categories__products '>
+                        <motion.i className='products-container'
+                            initial={{ x: -200, }}
+                            whileInView={{ x: 0 }}
+                            transition={{ duration: 0.5, ease: "easeInOut" }}>
+                            <span className='inside-container' >
+                                <h1 className='inside-container__title '>Parfumes</h1>
+                            </span>
+                            <img src={girl_parfume}
+                                alt="girl_parfume" />
+                        </motion.i>
+                        <motion.i
+                            initial={{ y: 200 }}
+                            whileInView={{ y: 0 }}
+                            transition={{ duration: 0.5, ease: "easeInOut" }}
+                            className='products-container'>
+                            <span className='inside-container'>
+                                <h1 className='inside-container__title '>Face</h1>
+                            </span>
+                            <img src={girl_face}
+                                alt="girl_face" />
+                        </motion.i>
+                        <motion.i className='products-container'
+                            initial={{ x: 200, }}
+                            whileInView={{ x: 0 }}
+                            transition={{ duration: 0.5, ease: "easeInOut" }}>
+                            <span className='inside-container'>
+                                <h1 className='inside-container__title '>Lips</h1>
+                            </span>
+                            <img src={girl_lips}
+                                alt="girl_lips" />
+                        </motion.i>
+                        <motion.i
+                            initial={{ y: 200 }}
+                            whileInView={{ y: 0 }}
+                            transition={{ duration: 0.5, ease: "easeInOut" }}
+                            className='products-container'>
+                            <span className='inside-container'>
+                                <h1 className='inside-container__title '>Acsesuars</h1>
+                            </span>
+                            <img src={girl_acsesuars}
+                                alt="girl_acsesuars" />
+                        </motion.i>
+                    </section>
+                </div>
             </main >
         </>
     )
